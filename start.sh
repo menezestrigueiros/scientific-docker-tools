d6765efadc310aec8da25427be075b80fee28210
André Filipe Arranca Menezes Trigueiros da Silva <156216447+menezestrigueiros@users.noreply.github.com>
2025-08-23 23:38:28 +0000

    test reproducibility by not giving the host directory as working_dir

--- start.sh
+++ start.sh
@@ -2,9 +2,11 @@
 set -e
 
 echo "[INFO] Allowing Docker containers to use X11 display..."
-xhost +local:docker > /dev/null
+#xhost +local:docker > /dev/null
 
 echo "[INFO] Starting scientific tools container..."
+# one of Docker Compose’s superpowers: If the image is not present locally, it pulls it from Docker Hub (or any registry you configured)
+# Then it starts the container(s) defined in docker-compose.yml.
 docker compose up -d
 
 # Here, you copy your token and paste it in the browser or vscode.
